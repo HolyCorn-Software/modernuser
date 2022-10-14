a0d5cc1587858878165b1fef2caf6378bfad14df
Son of Binary <akwotom@hcts.ml>
2022-10-14 10:46:00 +0100

    Added internal methods for managing user groups

--- group/terminals/internal.mjs
+++ group/terminals/internal.mjs
@@ -22,6 +22,39 @@ export default class UserGroupInternalMethods {
 
         /** @type {GroupDataController} */
         this[data_controller_symbol] = data_controller
+
+        /** @type {GroupDataController} */
+        this.data = new ProxyWrapper(data_controller)
+
+        /** @type {GroupMembershipController} */
+        this.membership = new ProxyWrapper(membership_controller)
+    }
+
+}
+
+
+/**
+ * All we are doing here is, making something that will remove the first argument of every method being executed
+ */
+class ProxyWrapper {
+
+    constructor(source) {
+        const path = arguments[1]
+
+        if (source == null || (typeof source !== 'object' && typeof source !== 'function')) {
+            return source
+        }
+
+        if (typeof source === 'function') {
+            return function () {
+                return source(...[...arguments].slice(1))
+            }
+        }
+        return new Proxy(source, {
+            get: (target, property, receiver) => {
+                return new ProxyWrapper(Reflect.get(target, property, receiver))
+            }
+        })
     }
 
 }
--- modernuser-changes.md
+++ modernuser-changes.md
@@ -1,9 +1,3 @@
 
 
-    Improved the description to help future developers use it better
-
-    Made the default borrowed widgets more generic
-
-    Improved how superuser permissions are granted to the first user of the system
-
-    Improved documentation of createPermission() methods
+    Added internal methods for managing user groups
